e1a9ded570f513715dc960a823fb094a21f89e2c
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-02-18 18:33:42 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  isTherePairAddend = false
+  for(let i = 0; i < array.length; i++) {
+    let difference = Math.abs(target - array[i]) 
+    for(let j = i + 1; j < array.length; j++) {
+      if(array[j] === difference) {
+        isTherePairAddend = true
+        break
+      }
+    }
+    if(isTherePairAddend === true) {
+      break
+    }
+  }
+  return isTherePairAddend
 }
 
 /* 
   Write the Big O time complexity of your function here
+  isTherePairAddend = fales // O(1)
+  for loop //O(n)
+  let difference = Math.abs(target - array[i])  // O(1)
+  for loop  //O(n)
+  if condition //O(1)
+  O(1) + O(n) + O(1) + O(n) + O(1) = O(2n + 3) => O(n)
 */
 
 /* 
   Add your pseudocode here
+  thereIsPairAddend flag is false
+  loop through array, subtract first element from target
+  search rest of array for difference
+  if there is difference then thereIsPairAddend is true
+  else subtract second number from target and search rest array.
 */
 
 /*
   Add written explanation of your solution here
+  loop through array, by starting first number and subtract from the target.
+  search for second addend through rest array.
+  when find it, return true, else return false
 */
 
 // You can run `node index.js` to view these console logs
